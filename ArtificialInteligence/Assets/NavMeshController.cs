1680a315069298b0b58ce7de333aa9e120605d95
AleixCas95 <leix_05@hotmail.com>
2020-10-11 16:51:57 +0200

    NavMeshAgent implemented

---
+++ ArtificialInteligence/Assets/NavMeshController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshController : MonoBehaviour
+{
+
+    public Transform objective;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        agent.destination = objective.position;
+    }
+}
