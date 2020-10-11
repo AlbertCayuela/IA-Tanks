8ab8b5cdd0e20ecbac30387b0208eba12dbd5385
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-11 19:36:36 +0200

    wander now only finds points in walkable mesh

--- ArtificialInteligence/Assets/Scripts/Tank/SteeringWander.cs
+++ ArtificialInteligence/Assets/Scripts/Tank/SteeringWander.cs
@@ -10,7 +10,9 @@ public class SteeringWander : MonoBehaviour
     public float offset = 3f;
     public Vector3 local_target;
     public Vector3 world_target;
-    public NavMeshAgent agent;
+    private NavMeshAgent agent;
+    private NavMeshHit hit;
+    public bool target_is_walkable;
     public float distance_to_target;
 
     // Start is called before the first frame update
@@ -22,7 +24,7 @@ public class SteeringWander : MonoBehaviour
         local_target = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
         local_target.Normalize();
         local_target *= radius;
-        local_target += new Vector3(0, 0, offset);
+        local_target += new Vector3(0, 0, Random.Range(10,20));
 
         world_target = transform.TransformPoint(local_target);
         world_target.y = 0f;
@@ -34,16 +36,16 @@ public class SteeringWander : MonoBehaviour
         //calculate distance to the next point the tank is going
         distance_to_target = Vector3.Distance(world_target, transform.position);
         //draw a debug line to check where is the point while doing the game
-        Debug.DrawLine(transform.position, world_target, Color.red);
+        Debug.DrawLine(transform.position, world_target, Color.green);
 
         //reduce amount of points calculated --> if distance is less than x, calculate new point
-        if (distance_to_target <= 3)
+        if (distance_to_target <= 3 || CheckIfWalkable(world_target))
         {
 
             local_target = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
             local_target.Normalize();
             local_target *= radius;
-            local_target += new Vector3(0, 0, offset);
+            local_target += new Vector3(0, 0, Random.Range(10,20));
 
             world_target = transform.TransformPoint(local_target);
             world_target.y = 0f;
@@ -51,6 +53,13 @@ public class SteeringWander : MonoBehaviour
 
         //set tank target as agent destination
         agent.destination = world_target;
+    }
+
+    bool CheckIfWalkable(Vector3 wolrd_target)
+    {
+        if (NavMesh.Raycast(transform.position, world_target, out hit, NavMesh.AllAreas))
+            return true;
+        else return false;
     }
  
 }
