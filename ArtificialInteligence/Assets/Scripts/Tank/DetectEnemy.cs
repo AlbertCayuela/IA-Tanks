85392caaf31911ba20f4d210ebf5c1e577e5e4b8
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-12 11:03:07 +0200

    if tank detects enemy, turret looks at it

--- ArtificialInteligence/Assets/Scripts/Tank/DetectEnemy.cs
+++ ArtificialInteligence/Assets/Scripts/Tank/DetectEnemy.cs
@@ -6,14 +6,16 @@ public class DetectEnemy : MonoBehaviour
 {
  
     public GameObject enemy;   //enemy tank
+    public GameObject turret; //tank turret
     public float range_to_detect = 4f; //range where enemy is gonna be detected
     public float distance_to_enemy = 0f; //distance from the tank to the enemy tank
     public bool enemy_detected = false; //bool to check if the tank detected the enemy tank
 
+ 
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,8 +24,14 @@ public class DetectEnemy : MonoBehaviour
         //calculate distance from tank to enemy tank
         distance_to_enemy = Vector3.Distance(enemy.transform.position, transform.position);
 
+        
+
         //if the enemy tank is in the detecting range --> enemy detected
         if (distance_to_enemy <= range_to_detect)
+        {
             enemy_detected = true;
+            turret.transform.LookAt(enemy.transform);
+        }
+           
     }
 }
