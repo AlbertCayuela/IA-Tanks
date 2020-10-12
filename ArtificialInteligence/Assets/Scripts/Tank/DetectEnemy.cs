338b803bb2a7caa320d81ee1814e187f10b6afdb
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-12 10:27:58 +0200

    Tank detects and follows tank if detected

---
+++ ArtificialInteligence/Assets/Scripts/Tank/DetectEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectEnemy : MonoBehaviour
+{
+ 
+    public GameObject enemy;   //enemy tank
+    public float range_to_detect = 4f; //range where enemy is gonna be detected
+    public float distance_to_enemy = 0f; //distance from the tank to the enemy tank
+    public bool enemy_detected = false; //bool to check if the tank detected the enemy tank
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate distance from tank to enemy tank
+        distance_to_enemy = Vector3.Distance(enemy.transform.position, transform.position);
+
+        //if the enemy tank is in the detecting range --> enemy detected
+        if (distance_to_enemy <= range_to_detect)
+            enemy_detected = true;
+    }
+}
---
+++ ArtificialInteligence/Assets/Scripts/Tank/TankStateManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankStateManager : MonoBehaviour
+{
+
+    public enum STATE {WANDERING, PATROLING, SEEKING, FLEEING, NULL};
+    public STATE current_state = STATE.NULL;
+
+    SteeringWander wander;
+    DetectEnemy detect_enemy;
+    SeekTarget seek;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wander = GetComponent<SteeringWander>();
+        detect_enemy = GetComponent<DetectEnemy>();
+        seek = GetComponent<SeekTarget>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current_state == STATE.WANDERING)
+            wander.enabled = true;
+        else
+            wander.enabled = false;
+
+        if (detect_enemy.enemy_detected == true)
+        {
+            current_state = STATE.SEEKING;
+            seek.enabled = true;
+        }
+        else
+            seek.enabled = false;
+    }
+}
