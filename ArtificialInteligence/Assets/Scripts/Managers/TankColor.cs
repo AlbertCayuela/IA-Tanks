60b8c809ce4610787037262b5c8bb81e2f5dfbde
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-19 17:44:06 +0200

    Color script working

---
+++ ArtificialInteligence/Assets/Scripts/Managers/TankColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankColor : MonoBehaviour
+{
+    public Color wander_color;
+    public Color patrol_color;
+
+    public GameObject wander_tank;
+    public GameObject patrol_tank;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MeshRenderer[] wander_renderers = wander_tank.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < wander_renderers.Length; i++)
+        {
+            wander_renderers[i].material.color = wander_color;
+        }
+
+        MeshRenderer[] patrol_renderers = patrol_tank.GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < patrol_renderers.Length; i++)
+        {
+            patrol_renderers[i].material.color = patrol_color;
+        }
+    }
+}
