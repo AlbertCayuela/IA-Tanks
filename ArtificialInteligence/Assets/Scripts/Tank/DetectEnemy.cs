52cb77643a1fd24d90bc7cac07fe5fb172730e29
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-24 17:40:13 +0200

    tankstate manager implemented

--- ArtificialInteligence/Assets/Scripts/Tank/DetectEnemy.cs
+++ ArtificialInteligence/Assets/Scripts/Tank/DetectEnemy.cs
@@ -6,8 +6,8 @@ public class DetectEnemy : MonoBehaviour
 {
  
     public GameObject enemy;   //enemy tank
-    public GameObject turret; //tank turret
-    public float range_to_detect = 4f; //range where enemy is gonna be detected
+    //public GameObject turret; //tank turret
+    public float range_to_detect; //range where enemy is gonna be detected
     public float distance_to_enemy = 0f; //distance from the tank to the enemy tank
     public bool enemy_detected = false; //bool to check if the tank detected the enemy tank
 
@@ -23,8 +23,6 @@ public class DetectEnemy : MonoBehaviour
     {
         //calculate distance from tank to enemy tank
         distance_to_enemy = Vector3.Distance(enemy.transform.position, transform.position);
-
-        
 
         //if the enemy tank is in the detecting range --> enemy detected
         if (distance_to_enemy <= range_to_detect)
