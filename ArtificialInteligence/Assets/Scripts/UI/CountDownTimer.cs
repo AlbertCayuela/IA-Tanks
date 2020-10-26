46bee8ca66b8599f62f2b992ed825a97287009c4
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-26 08:44:06 +0100

    winner text done and shoot unabled

---
+++ ArtificialInteligence/Assets/Scripts/UI/CountDownTimer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountDownTimer : MonoBehaviour
+{
+    public int countDownTimer;
+    public Text countDownDisplay;
+
+    public GameObject patrol_tank_manager;
+    public GameObject wander_tank_manager;
+
+    public Text timer_text;
+    Timer timer;
+
+    private void Start()
+    {
+        StartCoroutine(CountDown());
+        wander_tank_manager.GetComponent<TankStateManager>().enabled = false;
+        patrol_tank_manager.GetComponent<TankStateManager>().enabled = false;
+        timer = GetComponent<Timer>();
+        timer_text.gameObject.SetActive(false);
+    }
+
+    IEnumerator CountDown()
+    {
+        while(countDownTimer > 0)
+        {
+            countDownDisplay.text = countDownTimer.ToString();
+            yield return new WaitForSeconds(1f);
+
+            countDownTimer--;
+
+        }
+
+        countDownDisplay.text = "GO!";
+        yield return new WaitForSeconds(1f);
+        countDownDisplay.gameObject.SetActive(false);
+        wander_tank_manager.GetComponent<TankStateManager>().enabled = true;
+        patrol_tank_manager.GetComponent<TankStateManager>().enabled = true;
+        timer.enabled = true;
+        timer_text.gameObject.SetActive(true);
+    }
+}
---
+++ ArtificialInteligence/Assets/Scripts/UI/WinnerTextScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerTextScript : MonoBehaviour
+{
+
+    public Text text_winner;
+    public GameObject wander_tank;
+    public GameObject patrol_tank;
+    TankHealth wander_tank_health;
+    TankHealth patrol_tank_health;
+    Shoot patrol_shoot;
+    Shoot wander_shoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text_winner.gameObject.SetActive(false);
+        wander_tank_health = wander_tank.GetComponent<TankHealth>();
+        patrol_tank_health = patrol_tank.GetComponent<TankHealth>();
+        patrol_shoot = patrol_tank.GetComponent<Shoot>();
+        wander_shoot = wander_tank.GetComponent<Shoot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(wander_tank_health.m_CurrentHealth<=0 || patrol_tank_health.m_CurrentHealth <= 0)
+        {
+            text_winner.gameObject.SetActive(true);
+            patrol_shoot.enabled = false;
+            wander_shoot.enabled = false;
+        }
+    }
+}
