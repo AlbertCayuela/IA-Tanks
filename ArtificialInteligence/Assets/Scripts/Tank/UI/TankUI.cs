a1520dc8d716d1512c721646a965ac0185b2d7dc
AlbertCayuela <albertcayuela16@gmail.com>
2020-10-26 10:35:22 +0100

    reloading ui done(sometimes doesnt get deleted need to get fixed)

--- ArtificialInteligence/Assets/Scripts/Tank/UI/TankUI.cs
+++ ArtificialInteligence/Assets/Scripts/Tank/UI/TankUI.cs
@@ -7,15 +7,22 @@ public class TankUI : MonoBehaviour
 {
 
     public Image ui_detected;
-    Shoot shoot;
+    public Image ui_reloading;
 
+    Shoot shoot;
+    TankHealth tank_health;
+
+    private float count_down_detected;
     private float count_down_reload;
     // Start is called before the first frame update
     void Start()
     {
         shoot = GetComponent<Shoot>();
         ui_detected.gameObject.SetActive(false);
+        ui_reloading.gameObject.SetActive(false);
+        count_down_detected = 0f;
         count_down_reload = 0f;
+        tank_health = GetComponent<TankHealth>();
     }
 
     // Update is called once per frame
@@ -24,13 +31,17 @@ public class TankUI : MonoBehaviour
         //UICountDown();
        
         EnemyDetectedUI();
+        
+        ReloadingUI();
+
+
     }
 
     void EnemyDetectedUI()
     {
         if(shoot.enemy_detected)
         {
-            count_down_reload += Time.deltaTime;
+            count_down_detected += Time.deltaTime;
             ui_detected.gameObject.SetActive(true);
 
             float offset_pos_y = transform.position.y + 5f;
@@ -41,10 +52,34 @@ public class TankUI : MonoBehaviour
 
             ui_detected.transform.position = screen_point;
 
-            if (count_down_reload >= 2)
+            if (count_down_detected >= 2)
             {
                 ui_detected.gameObject.SetActive(false);
             }
         }
+    }
+
+    void ReloadingUI()
+    {
+        if (shoot.reloading)
+        {
+            count_down_reload += Time.deltaTime;
+            //ui_reloading.gameObject.SetActive(true);
+            float r_offset_pos_y = transform.position.y + 3f;
+            Vector3 r_offset_pos = new Vector3(transform.position.x, r_offset_pos_y, transform.position.z);
+            Vector2 r_screen_point = Camera.main.WorldToScreenPoint(r_offset_pos);
+            ui_reloading.transform.position = r_screen_point;
+            if(count_down_reload >= 0.5)
+            {
+                ui_reloading.gameObject.SetActive(true);
+                count_down_reload = 0f;
+            }
+        }
+        else if(!shoot.reloading)
+        {
+            ui_reloading.gameObject.SetActive(false);
+            count_down_reload = 0f;
+        }
+       
     }
 }
