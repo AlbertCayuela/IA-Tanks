adb39b1034e69c757354fb98b641b2ecc446a543
AlbertCayuela <albertcayuela16@gmail.com>
2020-12-07 21:10:24 +0100

    improving patrolling tree

---
+++ ArtificialInteligence/Assets/Scripts/BehaviourTreeCode/EnemyFarCondition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Pada1.BBCore;
+using Pada1.BBCore.Framework;
+
+[Condition("MyConditions/IsEnemyFar")]
+
+public class EnemyFarCondition : ConditionBase
+{
+    public override bool Check()
+    {
+        GameObject patrolling_tank = GameObject.Find("PatrolTank");
+        GameObject wandering_tank = GameObject.Find("WanderTank");
+        return Vector3.Distance(patrolling_tank.transform.position, wandering_tank.transform.position) > 20f;
+    }
+}
